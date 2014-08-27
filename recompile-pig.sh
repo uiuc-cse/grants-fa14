cdecc796ce7dfeb1ac476155b06607394d7bafb2
Neal Davis <davis68@illinois.edu>
2014-08-27 15:42:11 -0500

    Some cleanup.

--- recompile-pig.sh
+++
@@ -1,8 +0,0 @@
-cd /home/cse-user/Code/pig-0.12.1
-sudo JAVA_HOME=/usr/local/Java/jdk1.8.0_05/ ant -Dhadoopversion=23
-
-sudo cp -r /home/cse-user/Code/pig-0.12.1 /home/hduser/Code
-cd /home/hduser/Code/pig-0.12.1
-sudo chown -R hduser:hadoop .
-
-
